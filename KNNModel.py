f23252922709c9240be836de1188a5d9ec3a4c8d
perkyPupa <144793898+perkyPupa@users.noreply.github.com>
2023-09-14 00:01:02 +0530

    added some more methods

--- KNNModel.py
+++ KNNModel.py
@@ -6,78 +6,17 @@ Created on Fri Sep  1 01:17:12 2023
 """
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+import openai
 from flask import Flask, request, jsonify,render_template
-from sklearn.metrics import classification_report
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 import csv
-
-
-df=pd.read_csv("Crop_recommendation.csv")
-
-c=df.label.astype('category')
-
-df['target']=c.cat.codes
-targets = dict(enumerate(c.cat.categories))
-
-y=df.target
-X=df[['N','P','K','temperature','humidity','ph','rainfall']]
-
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
-
-scaler_instance = MinMaxScaler()
-scaler_instance.fit(X_train)
-
-X_train_scaled = scaler_instance.transform(X_train)
-
-X_test_scaled = scaler_instance.transform(X_test)
 
 
-knn_instance = KNeighborsClassifier()
 
-knn_instance.fit(X_train_scaled,y_train)
-predictions_with_model=knn_instance.predict(X_test_scaled)
-from sklearn import metrics
-metrics.accuracy_score(y_test, predictions_with_model)
-knn_instance.score(X_test_scaled, y_test)
-
-from sklearn.metrics import confusion_matrix
-mat=confusion_matrix(y_test,knn_instance.predict(X_test_scaled))
-df_cm = pd.DataFrame(mat, list(targets.values()), list(targets.values()))
-sns.set(font_scale=1.0) # for label size
-plt.figure(figsize = (12,8))
-sns.heatmap(df_cm, annot=True, annot_kws={"size": 12},cmap="terrain")
-
-k_range = range(1,11)
-scores = []
-
-for k in k_range:
-    knn = KNeighborsClassifier(n_neighbors = k)
-    knn.fit(X_train_scaled, y_train)
-    scores.append(knn.score(X_test_scaled, y_test))
-
-
-plt.xlabel('k')
-plt.ylabel('accuracy')
-plt.scatter(k_range, scores)
-plt.vlines(k_range,0, scores, linestyle="dashed")
-plt.ylim(0.96,0.99)
-plt.xticks([i for i in range(1,11)]);
-
-# =============================================================================
-# X_input=[]
-# print("Enter the values of N P K Temp Humidity PH and Rainfall space separated")
-# for ele in input().split():
-#   X_input.append(float(ele))
-# X_input_scaled = scaler_instance.transform(pd.DataFrame(np.array(X_input)).T)
-# print("Prediction Crop is",targets[knn_instance.predict(X_input_scaled)[0]])
-# =============================================================================
 
 crop=""
+cropList=[]
 
 app = Flask(__name__)
 @app.route("/")
@@ -86,6 +25,15 @@ def index():
 
 @app.route("/predict",methods=['POST'])
 def predict():
+    df=pd.read_csv("Crop_recommendation.csv")
+    c=df.label.astype('category')
+    df['target']=c.cat.codes
+    targets = dict(enumerate(c.cat.categories))
+    y=df.target
+    X=df[['N','P','K','temperature','humidity','ph','rainfall']]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    gaussian_naive_bayes_instance=GaussianNB()
+    gaussian_naive_bayes_instance.fit(X_train,y_train)
     N = request.form['Nitrogen']
     P = request.form['Phosporus']
     K = request.form['Potassium']
@@ -95,13 +43,26 @@ def predict():
     rainfall = request.form['Rainfall']
 
     feature_list = [N, P, K, temp, humidity, ph, rainfall]
-    global crop 
-
+    global crop
+    global cropList
     try:
-        X_input_scaled = scaler_instance.transform(pd.DataFrame(np.array(feature_list)).T)
-        crop = targets[knn_instance.predict(X_input_scaled)[0]]
-    except : 
-        print ("Exception Occured")
+        X_input = pd.DataFrame(np.array(feature_list)).T
+        gaussian_naive_bayes_instance.predict(X_input)
+        my_prob_predictions=gaussian_naive_bayes_instance.predict_proba(X_input)
+        sorted_classes=np.argsort(my_prob_predictions[0])
+        best_class=sorted_classes[-1]
+        second_best_class=sorted_classes[-2]
+        third_best_class=sorted_classes[-3]
+        predictedcrop=targets[best_class]
+        firstAlternateCrop=targets[second_best_class]
+        secondAlternateCrop=targets[third_best_class]
+        print("The First Prediction is ",predictedcrop)
+        print("The Second Prediction is ",firstAlternateCrop)
+        print("The Third Prediction is ",secondAlternateCrop)
+        crop=predictedcrop
+        cropList =[predictedcrop,firstAlternateCrop,secondAlternateCrop]
+    except: 
+        print ("Exception Occured in predict method")
         crop=None;
        
     if crop is not None:
@@ -112,15 +73,50 @@ def predict():
 
 
 
-companionCrop=""
+@app.route("/findAlternateCrop",methods=['POST'])
+def findAlternateCrop():
+    global cropList
+    try:
+      print("The Second Prediction is ",cropList[1])
+      print("The Third Prediction is ",cropList[2])
+    except:
+      print ("Exception Occured in findAlternateCrop method")
+      cropList=None;
+        
+    if cropList is not None:
+        findAlternateCropResult = cropList
+    else:
+        findAlternateCropResult = []
+    return render_template('index.html',findAlternateCropResult = findAlternateCropResult)
 
+
+@app.route("/callGPT",methods=['POST'])
+def callGPT():
+    try:
+        openai.api_key = "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"
+        prompt = request.form['promptFromUser']
+        model = "text-davinci-003"
+        response = openai.Completion.create(engine=model, prompt=prompt, max_tokens=4000)
+        generated_text = response.choices[0].text
+        print(generated_text)
+    except:
+      print ("Exception Occured in callGPT method")
+      generated_text=None;
+    if generated_text is not None:
+        generated_textFromGPT = generated_text
+    else:
+        generated_textFromGPT = "Sorry ChatGPT is not available please try after some time."
+    return render_template('index.html',generated_textFromGPT = generated_textFromGPT)
+
+
+companionCrop=""
 
 
 @app.route("/findCompanionCrop",methods=['POST'])
 def findCompanionCrop():
     predictedcrop = crop 
     selectedAdditionalFactor = request.form.get("selectedAdditionalFactor")
-   
+    global companionCrop
     csv_file = "companion_crop_data.csv"
     if predictedcrop==" ":
         predictedcrop=None
@@ -137,19 +133,21 @@ def findCompanionCrop():
                     print(predictedcrop)
                     if row["additional factor"] == selectedAdditionalFactor:
                         companionCrop=str(row["companion crop"])
-                        print(companionCrop)
+                        print(companionCrop)              
     except: 
-          print ("Exception Occured")
+          print ("Exception Occured in findCompanionCrop method")
           companionCrop=None;
           
     print(companionCrop)      
     if companionCrop is not None:
-      companionCropResult = "To improve the selected factor you can plant {} along with main crop.".format(companionCrop.upper())
+      companionCropResult = "To improve the selected factor you can plant {} along with the main crop.".format(companionCrop.upper())
     else:
        companionCropResult = "Sorry, we could not determine the companion crop"       
     return jsonify({"companionCropResult": companionCropResult})
 
 
+
+
 # python main
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=80)
